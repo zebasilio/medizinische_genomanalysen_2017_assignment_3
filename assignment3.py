5141f13c23a37820decd8551dd3164a3a259a448
zebasilio <zebasilio@gmail.com>
2017-04-21 21:21:50 +0200

    commit

--- assignment3.py
+++ assignment3.py
@@ -1,57 +1,76 @@
 #! /usr/bin/env python2
 
+'''
+Before running the script, change the path of the vcf files in the files variable (line 16, 17 and 18)
+to run the script, in the command line:
+$chmod +x assignment3.py
+$./assignment3.py > assignment3.txt
+'''
+
 import vcf
+from vcf import utils
 import hgvs
 
-__author__ = 'XXX'
+__author__ = 'Jose Basilio'
 
+file_mother = "/home/jose/MedGenAn/medizinische_genomanalysen_2017_assignment_3/AmpliseqExome.20141120.NA24143.vcf"
+file_father = "/home/jose/MedGenAn/medizinische_genomanalysen_2017_assignment_3/AmpliseqExome.20141120.NA24149.vcf"
+file_son = "/home/jose/MedGenAn/medizinische_genomanalysen_2017_assignment_3/AmpliseqExome.20141120.NA24385.vcf"
 
 class Assignment3:
     
     def __init__(self):
         ## Check if pyvcf is installed
-        print("PyVCF version: %s" % vcf.VERSION)
+        print "PyVCF version: %s" % vcf.VERSION
         ## Check if hgvs is installed
-        print("HGVS version: %s" % hgvs.__version__)
+        print "HGVS version: %s" % hgvs.__version__
         
 
     def get_total_number_of_variants_mother(self):
-        '''
-        Return the total number of identified variants in the mother
-        :return: 
-        '''
-        print("TODO")
-        
+        print "\n+++++++++++++++++++\nReturn the total number of identified variants in the mother:"
+        count = 0
+        for line in vcf.Reader(open(file_mother, "r")):
+            count += 1
+        print count
+        return count
+            
         
     def get_total_number_of_variants_father(self):
-        '''
-        Return the total number of identified variants in the father
-        :return: 
-        '''
-        print("TODO")
+       print "\n+++++++++++++++++++\nReturn the total number of identified variants in the father:"
+       count = 0
+       for line in vcf.Reader(open(file_father, "r")):
+            count += 1
+       print count
+       return count
        
         
     def get_variants_shared_by_father_and_son(self):
-        '''
-        Return the number of identified variants shared by father and son
-        :return: 
-        '''
-        print("TODO")
+       print "\n+++++++++++++++++++\nReturn the number of identified variants shared by father and son:"
+       count = 0
+       for line in vcf.Reader(open(file_father, "r")):
+           if line in vcf.Reader(open(file_son, "r")):
+               count += 1
+       print count
+       return count
         
         
     def get_variants_shared_by_mother_and_son(self):
-        '''
-        Return the number of identified variants shared by mother and son
-        :return: 
-        '''
-        print("TODO")
+        print "\n+++++++++++++++++++\nReturn the number of identified variants shared by mother and son:"
+        count = 0
+        for line in vcf.Reader(open(file_mother, "r")):
+        	if line in vcf.Reader(open(file_son, "r")):
+                	count += 1
+        print count
+        return count
         
     def get_variants_shared_by_trio(self):
-        '''
-        Return the number of identified variants shared by father, mother and son
-        :return: 
-        '''
-        print("TODO")
+        print "\n+++++++++++++++++++\nReturn the number of identified variants shared by father, mother and son:"
+        count = 0
+        for line in vcf.Reader(open(file_father, "r")):
+		if line in vcf.Reader(open(file_mother, "r")) and line in vcf.Reader(open(file_son, "r")):
+			count += 1
+        print count
+        return count
         
 
     def merge_mother_father_son_into_one_vcf(self):
@@ -59,7 +78,13 @@ class Assignment3:
         Creates one VCF containing all variants of the trio (merge VCFs)
         :return: 
         '''
-        print("TODO")
+        merge = open ("merge.vcf", "w")
+        writer = vcf.Writer(merge, vcf.Reader(open(file_mother, "r")), "\n")
+        for lines in utils.walk_together(vcf.Reader(open(file_mother, "r")), vcf.Reader(open(file_father, "r")), vcf.Reader(open(file_son, "r"))):
+            for entry in lines:
+                if entry is not None:
+                    writer.write_record(entry)
+        print "merged files"
         
         
     def convert_first_variants_of_son_into_HGVS(self):
@@ -69,11 +94,17 @@ class Assignment3:
         - https://hgvs.readthedocs.io/en/master/examples/manuscript-example.html#project-genomic-variant-to-a-new-transcript
         :return: 
         '''
-        print("TODO")
+        print "do not know how to do it"
         
     
     def print_summary(self):
-        print("Print all results here")
+        #self.get_total_number_of_variants_mother()
+        #self.get_total_number_of_variants_father()
+        self.get_variants_shared_by_father_and_son()
+        #self.get_variants_shared_by_mother_and_son()
+        #self.get_variants_shared_by_trio()
+        #self.merge_mother_father_son_into_one_vcf()
+        #self.convert_first_variants_of_son_into_HGVS()
     
         
 if __name__ == '__main__':
